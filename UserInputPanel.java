68319a44ef9143193dd931381cce4f5610c34b0f
JamieABall <jamiegamer93@att.net>
2016-11-18 15:15:18 -0800

    Update UserInputPanel.java

--- UserInputPanel.java
+++ UserInputPanel.java
@@ -2,7 +2,7 @@
 Class: UserInputPanel.java
 Custom UI element for URL text field
 @Author: Jamison Ball
-@Date: November 17th, 2016
+@Date Written: November 17th, 2016
 @Last Updated: November 17th, 2016
 *********************************************************************************************************************/
 import java.awt.FlowLayout;
