842a183e2643ea3471fe3328ef9ddba8ae7e0493
JamieABall <jamiegamer93@att.net>
2016-11-17 17:10:01 -0800

    Create ApplicationDriver.java

---
+++ ApplicationDriver.java
@@ -0,0 +1,24 @@
+/******************************************************************************************************************************
+Class:ApplicationDriver.java
+The inital entry point of the application.
+@Author: Jamison Ball
+@Date: November 17th, 2016
+@Last Updated: November 17th, 2016
+*******************************************************************************************************************************/
+import java.net.URL;
+import java.net.URI;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.io.IOException;
+
+public class ApplicationDriver {
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				AppUI instance = new AppUI();
+			}
+		});
+	}
+}
